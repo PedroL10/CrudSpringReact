655f3f87d9df1a33ec18e734a64ec071f6d4b430
Pedro Goncalves <pepluisgon01@gmail.com>
2024-05-24 09:23:43 -0300

    Classe StudentCOntroller

--- src/main/java/com/crud/demo/controller/StudentController.java
+++ src/main/java/com/crud/demo/controller/StudentController.java
@@ -1,9 +1,13 @@
 package com.crud.demo.controller;
 
+import com.crud.demo.model.Student;
 import com.crud.demo.service.IStudentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/students")
@@ -13,5 +17,30 @@ public class StudentController {
 
     public StudentController(IStudentService studentService) {
         this.studentService = studentService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Student>> getStudents() {
+        return new ResponseEntity<>(studentService.getStudents(), HttpStatus.FOUND);
+    }
+
+    @PostMapping
+    public Student addStudent(@RequestBody Student student) {
+        return  studentService.addStudent(student);
+    }
+
+    @PutMapping("/update/{id}")
+    public Student updateStudent(@RequestBody Student student, @PathVariable Long id){
+        return studentService.updateStudent(student,id);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteStudent(@PathVariable Long id){
+        studentService.deleteStudent(id);
+    }
+
+    @GetMapping("student/{id}")
+    public Student getStudent(@PathVariable Long id){
+        return  studentService.getStudentById(id);
     }
 }
